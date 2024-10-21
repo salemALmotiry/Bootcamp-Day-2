e4c7f36281ad0eda5c930c53331e75ab58ac8ded
salem ALmotiry <60990492+salemALmotiry@users.noreply.github.com>
2024-10-21 18:38:00 +0300

    Day 2

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// run each exercise separately
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
